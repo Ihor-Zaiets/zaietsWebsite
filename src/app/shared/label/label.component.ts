1913576ab39d6e59c05bd70ca9f4c938fd021d18
Ihor Zaiets <dekstroer8202@gmail.com>
2024-07-03 17:04:38 +0200

    create label and input field shared components and add them to shared.module.ts

---
+++ src/app/shared/label/label.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-label',
+  templateUrl: './label.component.html',
+  styleUrl: './label.component.scss'
+})
+export class LabelComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { LabelComponent } from './label/label.component';
+import { InputFieldComponent } from './input-field/input-field.component';
 
 @NgModule({
   declarations: [
     ButtonComponent,
+    LabelComponent,
+    InputFieldComponent,
   ],
   imports: [
     CommonModule,
   ],
   exports: [
     ButtonComponent,
+    LabelComponent,
+    InputFieldComponent,
   ]
 })
 export class SharedModule { }
