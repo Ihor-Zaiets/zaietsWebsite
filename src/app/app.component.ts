efad3496637e122ef880aa3bd62edd38b20d3ca5
Ihor Zaiets <dekstroer8202@gmail.com>
2024-07-10 10:27:50 +0200

    delete unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { SkillsComponent } from './home/skills/skills.component';
-import { ProjectsComponent } from './home/projects/projects.component';
-import { AboutMeComponent } from './home/about-me/about-me.component';
-import { ContactComponent } from './home/contact/contact.component';
-import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    SharedModule,
-    HomeComponent,
-    SkillsComponent,
-    ProjectsComponent,
-    AboutMeComponent,
-    ContactComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
