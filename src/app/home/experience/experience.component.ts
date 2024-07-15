6bd7cde197a4143f441ac381cc809e2f73cbe66e
Ihor Zaiets <dekstroer8202@gmail.com>
2024-07-15 17:14:27 +0200

    create experience page

--- src/app/home/experience/experience.component.ts
+++ src/app/home/experience/experience.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {SharedModule} from "../../shared/shared.module";
+import {ButtonType} from "../../shared/button/button.component";
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [
+    SharedModule
+  ],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.scss'
 })
 export class ExperienceComponent {
 
+  protected readonly ButtonType = ButtonType;
 }
