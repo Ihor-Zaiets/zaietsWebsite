ccac4be35a8435d5b2f211ee10a6810773e36ef8
Ihor Zaiets <dekstroer8202@gmail.com>
2024-11-19 19:58:58 +0100

    set up spring boot web application launch

---
+++ back_end/src/main/java/ihor/zaiets/ZaietsWebsiteApplication.java
@@ -0,0 +1,11 @@
+package ihor.zaiets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaietsWebsiteApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ZaietsWebsiteApplication.class, args);
+  }
+}
