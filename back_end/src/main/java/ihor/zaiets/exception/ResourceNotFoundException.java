c6bc004870b0bd0a4c91da2487f83b565b7d4e33
Ihor Zaiets <dekstroer8202@gmail.com>
2024-11-20 22:26:10 +0100

    create ResourceNotFoundException class

---
+++ back_end/src/main/java/ihor/zaiets/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package ihor.zaiets.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException() {
+        super("Resource not found.");
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
