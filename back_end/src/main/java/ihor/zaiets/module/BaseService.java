5d995bf814357f01da90e0759044424de3119253
Ihor Zaiets <dekstroer8202@gmail.com>
2024-11-20 22:26:22 +0100

    create BaseService class

---
+++ back_end/src/main/java/ihor/zaiets/module/BaseService.java
@@ -0,0 +1,29 @@
+package ihor.zaiets.module;
+
+import ihor.zaiets.exception.ResourceNotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BaseService<T, ID, DAO extends JpaRepository<T, ID>> {
+
+    protected DAO dao;
+
+    public T saveEntity(T t) {
+        return dao.save(t);
+    }
+
+    public T findById(ID id) {
+        return dao.findById(id).orElseThrow(ResourceNotFoundException::new);
+    }
+
+    public void deleteById(ID id) {
+        dao.deleteById(id);
+    }
+
+    public List<T> findAll() {
+        return dao.findAll();
+    }
+}
