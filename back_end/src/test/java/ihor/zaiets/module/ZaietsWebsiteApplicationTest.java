6d7b28c7a0b66438933c3cdde6de9cd74589ba8b
Ihor Zaiets <dekstroer8202@gmail.com>
2024-11-21 18:41:20 +0100

    add testing dependencies and project tests init

---
+++ back_end/src/test/java/ihor/zaiets/module/ZaietsWebsiteApplicationTest.java
@@ -0,0 +1,13 @@
+package ihor.zaiets.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ZaietsWebsiteApplicationTest {
+
+    @Test
+    void contextLoad() {
+
+    }
+}
