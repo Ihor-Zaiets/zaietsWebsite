e9cdce6ff5174b538189cb804bf525cd6c508d18
Ihor Zaiets <dekstroer8202@gmail.com>
2024-11-28 17:19:54 +0100

    translate all content on website

---
+++ back_end/src/main/resources/db.migration/V3__translate_all_content.sql
@@ -0,0 +1,39 @@
+call create_translation('home.myname', 'Ihor Zaiets', 'Ihor Zaiets', 'Игорь заец', 'Ігор Заєць');
+call create_translation('home.title', 'Fullstack developer', 'Developer fullstack', 'Разработчик fullstack', 'Розробник fullstack');
+
+call create_translation('skills.title.page', 'Skills', 'Stack technologiczny', '', 'Вміння');
+call create_translation('skills.title.backend', 'Backend', 'Backend', '', 'Бекенд');
+call create_translation('skills.title.frontend', 'Frontend', 'Frontend', '', 'Фронтенд');
+call create_translation('skills.title.other', 'Other', 'Inne', '', 'Інше');
+call create_translation('skills.title.languages', 'Languages', 'Języki', '', 'Мови');
+call create_translation('skills.title.soft_skills', 'Soft skills', 'Miękkie umiejętności', '', 'М''які навички');
+
+call create_translation('experience.title.page', 'Experience', 'Doświadczenie', 'Опыт работы', 'Досвід');
+
+call create_translation('about_me.title.page', 'About me', 'O mnie', 'Про меня', 'Про мене');
+call create_translation('about_me.content',
+    'I have 3 years of experience of commercial programming as back end Java developer. My main tech stack is Java, spring, hibernate, postgres and docker. I understand the importance of code testing. As developer I write unit and integration tests using JUnit5 and Mockito.
+Currently I’m creating my own brand web site which would nicely combine practical usability, my back end, front end and cloud computing knowledge.
+Generally I understand the value of human relationships and I’m able to be a part of a team. In my job I often needed to work with front end and analytics teams to solve a more complex problems.',
+    'Mam 3 lata doświadczenia komercyjnego jako Java backend developer, pracując nad projektami opartymi na architekturze REST API. Mój główny stack technologiczny obejmuje Javę, Spring, Hibernate, PostgreSQL i Dockera. Rozumiem ważność testowania kodu. W swojej prace pisałem testy jednostkowe i integracyjne używając JUnit5 i Mockito.
+Obecnie tworzę swoją stronę internetową jako część budowania osobistej marki. Ten projekt łączy w sobie elementy backendu, frontend developmentu oraz cloud computingu, co daje mi możliwość wszechstronnego rozwoju.
+Cenię sobie współpracę w zespole i rozumiem znaczenie relacji międzyludzkich w pracy. W mojej dotychczasowej roli często współpracowałem z zespołami frontendowymi i analitykami, aby wspólnie rozwiązywać bardziej złożone problemy.',
+    '',
+    '');
+
+call create_translation('contact.title.page', 'Contact', 'Kontakt', 'Контакт', 'Контакт');
+call create_translation('contact.content',
+    'I will respond as soon, as possible.
+Email: zaiets8202@gmail.com
+Location: Wroclaw, Poland',
+    'Odpowiem jak najszybszej.' ||
+    'Email: zaiets8202@gmail.com' ||
+    'Lokalizacja: Wrocław, Polska',
+    'Отвечу как можно быстрее.' ||
+    'Email: zaiets8202@gmail.com' ||
+    'Месторасположение: Вроцлав, Польша',
+    'Відповім так швидко, як тільки можливо.' ||
+    'Email: zaiets8202@gmail.com' ||
+    'Місцеположення: Вроцлав, Польша');
+
+
